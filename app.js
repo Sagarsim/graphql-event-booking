be2022945f7c0fa909ba5d709d31a30e77af5d84
sagarsim <sagar199785@gmail.com>
2020-06-07 14:42:00 +0530

    project init

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const {buildSchema} = require("graphql");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use("/graphql", graphqlHttp({
+schema: buildSchema(`
+
+type RootQuery {
+	events: [String!]!
+}
+
+type RootMutation {
+	createEvent(name: String!): String!
+}
+
+
+schema {
+	query: RootQuery
+	mutation: RootMutation
+}
+
+
+`),
+rootValue: {
+	events: () => {
+	var events = ["birthbay celebration", "cricket match", "guest lecture"];
+	return events;
+	},
+	createEvent: (args) => {
+		return args.name
+	}
+},
+graphiql: true
+}))
+
+app.listen(3000)
