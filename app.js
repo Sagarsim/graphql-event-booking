8cac8af995525243b84212916d85d469d0c438fc
sagarsim <sagar199785@gmail.com>
2020-06-27 17:22:25 +0530

    types added in schema

--- app.js
+++ app.js
@@ -6,15 +6,32 @@ const {buildSchema} = require("graphql");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.use("/graphql", graphqlHttp({
 schema: buildSchema(`
 
+type Event {
+	_id: ID!
+	title: String!
+	description: String!
+	price: Float!
+	date: String!
+}
+
+input EventInput {
+	title: String!
+	description: String!
+	price: Float!
+	date: String!
+}
+
 type RootQuery {
-	events: [String!]!
+	events: [Event!]!
 }
 
 type RootMutation {
-	createEvent(name: String!): String!
+	createEvent(eventInput: EventInput): Event
 }
 
 
@@ -27,11 +44,18 @@ schema {
 `),
 rootValue: {
 	events: () => {
-	var events = ["birthbay celebration", "cricket match", "guest lecture"];
 	return events;
 	},
 	createEvent: (args) => {
-		return args.name
+		const obj = {
+			_id: Math.random().toString(),
+			title: args.eventInput.title,
+			description: args.eventInput.description,
+			price: args.eventInput.price,
+			date: args.eventInput.date
+		}
+		events.push(obj)
+		return obj
 	}
 },
 graphiql: true
